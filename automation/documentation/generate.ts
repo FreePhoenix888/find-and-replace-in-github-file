c08a6e8e253d6d9afe42e9caa6c88c4a7f31c92f
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-06 13:17:08 +0600

    Use chmod to cli files

--- automation/documentation/generate.ts
+++ automation/documentation/generate.ts
@@ -1,8 +1,16 @@
 import { generateDocumentation } from "@deep-foundation/npm-automation";
+import fsExtra from 'fs-extra'
+import path from 'path'
 
 main();
 
 async function main() {
+  const files = fsExtra.readdirSync(path.resolve(process.cwd(), 'dist', 'cli'));
+  const writeMode = '755'
+  for (const file of files) {
+    fsExtra.chmodSync(file, writeMode)
+  }
+
   await generateDocumentation({
     generateCliAppsHelpInReadmeOptions: undefined,
     generateUsageWaysOfNpmCliAppsInMarkdownFormatOptions: undefined,
